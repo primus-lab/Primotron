6052276903942d70c4ff235370f7b3cbe1d5a75e
PedjaTerzic <tersit26@gmail.com>
2020-05-11 15:30:22 +0200

    My first commit

---
+++ src/primotron/Primotron.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primotron;
+
+/**
+ *
+ * @author pedja
+ */
+public class Primotron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
